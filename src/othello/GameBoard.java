3c093215dbc60408d91345d24cd5baa20b14ac35
Dominik <Dominik@Dominik-PC>
2016-05-05 22:59:49 +0200

    commit

--- src/othello/GameBoard.java
+++ src/othello/GameBoard.java
@@ -8,6 +8,7 @@ package othello;
 import game.Game;
 import game.Save;
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -108,7 +109,7 @@ public class GameBoard extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(29, 29, 29)
                 .addComponent(PlayingBoard, javax.swing.GroupLayout.PREFERRED_SIZE, 849, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 129, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addComponent(BoardGameSaveButton, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE)
                     .addComponent(BoardGameUndoButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -126,7 +127,7 @@ public class GameBoard extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(PlayingBoard, javax.swing.GroupLayout.PREFERRED_SIZE, 716, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(30, Short.MAX_VALUE))
+                .addContainerGap(100, Short.MAX_VALUE))
         );
 
         pack();
@@ -162,7 +163,7 @@ public class GameBoard extends javax.swing.JFrame {
         for(int row = 0; row < size; row++) {
             for(int col = 0; col < size; col++) {
                 JButton cell = new JButton();
-                cell.setBackground(Color.gray);
+                cell.setBackground(Color.green);
                 cell.setPreferredSize(new Dimension(50, 50));
                 
                 box.fill = GridBagConstraints.HORIZONTAL;
